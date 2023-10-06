8b11b44beba90393d9df47bffec19a259e678c4c
rosie <69148147+girlier@users.noreply.github.com>
2023-10-06 13:33:44 +0100

    Update Week2.py

--- Weeks/Week2.py
+++ Weeks/Week2.py
@@ -1,30 +1,43 @@
 # All the exercies from week 2 #
 
+# ▄▄▌ ▐ ▄▌▄▄▄ .▄▄▄ .▄ •▄     ▄▄▄▄▄▄▄▌ ▐ ▄▌      
+# ██· █▌▐█▀▄.▀·▀▄.▀·█▌▄▌▪    •██  ██· █▌▐█▪     
+# ██▪▐█▐▐▌▐▀▀▪▄▐▀▀▪▄▐▀▀▄·     ▐█.▪██▪▐█▐▐▌ ▄█▀▄ 
+# ▐█▌██▐█▌▐█▄▄▌▐█▄▄▌▐█.█▌     ▐█▌·▐█▌██▐█▌▐█▌.▐▌
+#  ▀▀▀▀ ▀▪ ▀▀▀  ▀▀▀ ·▀  ▀     ▀▀▀  ▀▀▀▀ ▀▪ ▀█▄▀▪
+
 def name():
     # Prints a result of 'hello {name}'
-    return "Hello, " + input('Hello, what is your name?\n >>>' + "Good to meet you!")
+    YourName = input('Hello, what is your name?\n >>>')
+    print(f'Hello, {YourName}  Good to meet you!')
 
 
 def celsius2fahrenheit():
-    # Prints a result of 38.4 conversion celsius to fahrenheit with the standard equation (C * (9/5)) + 32
-    temp = input('What is the temprature in Celsius? \n >>> ')
-    return str(temp) + ' Celsius is ' + str(temp * (9 / 5) + 32) + ' Fahrenheit'
+    # Prints a result of 38.4 conversion Celsius to Fahrenheit with the standard equation (C * (9/5)) + 32
+    Celsius = float(input('What is the temperature in Celsius? \n >>> '))
+    Fahrenheit = Celsius * 9 / 5 + 32
+    print(f'{Celsius} Celsius is {Fahrenheit} Fahrenheit')
 
 
 def poppleton():
+    # Gets the number of Students and size per lab #
+    LabSize = float(input('Input The Size of Lab\n >>>'))
+    NoStudents = float(input('Input The Number of Students\n >>>'))
+    NumberOfGroups = int(NoStudents // LabSize)
+    LeftOver = int(NoStudents % LabSize)
+
     # Prints how many groups can be made with each size of students with how many left any students
-    LabSize = input('Input The Size of Lab\n >>>')
-    NoStudents = input('Input The Number of Students\n >>>')
-    NumberOfGroups = NoStudents // LabSize
-    LeftOver = NoStudents % LabSize
-    print('There will be' + str(NumberOfGroups) + ' groups with ' + str(LeftOver) + ' left over\n')
+    print(f'There will be {NumberOfGroups} groups with {LeftOver} left over\n')
 
 
 def Sweets():
-    # prints the amount a teacher would have to evenly split sweets across the class
-    NoStudents = input('How many students do you have?\n >>>')
-    NoSweets = input('How many sweets do you have?\n >>>')
+    # Get the number of students and sweets as integers
+    NoStudents = int(input('How many students do you have?\n >>>'))
+    NoSweets = int(input('How many sweets do you have?\n >>>'))
+
+    # Calculate the sweets per student and the remaining sweets
     SweetsPP = NoSweets // NoStudents
     SweetsLO = NoSweets % NoStudents
-    return 'As the Teacher you would have to give each student ' + str(SweetsPP) + ' and you would have ' + str(
-        SweetsLO) + ' left over'
+
+    # Prints the result as a string
+    print(f'As the Teacher, you would have to give each student {SweetsPP} you would have {SweetsLO} left over')
